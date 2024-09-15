02e26008aa5c07e7be9c9c875d1a0c4ab2cd67f4
Hritik Gehlot <hgehlot1007@gmail.com>
2024-09-15 19:34:55 +0530

    Create Odd_even.py

---
+++ Odd_even.py
@@ -0,0 +1,6 @@
+print("Welcome let's check the number you are entering is a odd number or a even number\n")
+a = int(input("Please enter the number below\n"))
+if a%2==0:
+    print("This is a even number")
+else: print("This is a odd number")    
+
